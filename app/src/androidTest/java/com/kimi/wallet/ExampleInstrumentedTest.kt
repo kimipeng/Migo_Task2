8349c134ac36880afe25c98929e8568b8c14e39d
kimipeng <pon0306@gmail.com>
2020-08-13 17:42:45 +0800

    Add UI Test.

--- app/src/androidTest/java/com/kimi/wallet/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kimi/wallet/ExampleInstrumentedTest.kt
@@ -1,24 +1,38 @@
 package com.kimi.wallet
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.kimi.wallet.ui.add.AddPassActivity
+import com.kimi.wallet.ui.main.MainActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@LargeTest
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: IntentsTestRule<MainActivity> = IntentsTestRule(MainActivity::class.java)
+
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kimi.mediapass", appContext.packageName)
+        Espresso.onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(AddPassActivity::class.java.name))
     }
 }
